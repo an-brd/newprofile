d481927bd35fca32857c9aef51de26fbd2f9216b
an-brd <94487869+an-brd@users.noreply.github.com>
2022-04-04 19:41:52 +0100

    More changes

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 "use strict"
 let sections=document.getElementsByTagName("section")
 
-// hideSections()
-// displaySection("about-page")
-
-
-// hideSections()
-// displaySection ("s1")
 function hideSections() {
     for (let i=0;i<sections.length;i++) {
         sections[i].style.display="none"
